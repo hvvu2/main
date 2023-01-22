0129598eec21c573bd39c3d6ac104a589812c4ca
Ian <s5354555@gmail.com>
2023-01-22 14:49:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
+<script lang="ts" setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
